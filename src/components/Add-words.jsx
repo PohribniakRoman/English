cbdcc512f86336afbe0c682606b2bdfac6bd4b12
Roman <roman.pohribniak@gmail.com>
2021-04-12 23:13:44 +0300

    add-word

---
+++ src/components/Add-words.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+export default function AddWords() {
+  const [word, setWord] = useState("");
+  const [translation, setTranslation] = useState("");
+  const [explaining, setExplaining] = useState("");
+  const [isOpen, setOpen] = useState(false);
+  return (
+    <div className="add-words__wrapper">
+      <span>
+        <div className="add-words__container">
+          <h1>
+            Here you can add <br /> a new word to learn it!
+          </h1>
+        </div>
+        <form
+          className="add-words"
+          onSubmit={(event) => {
+            event.preventDefault();
+            console.log(translation, word, explaining);
+            setWord("");
+            setTranslation("");
+            setExplaining("");
+          }}
+        >
+          <h1>Create new entry!</h1>
+          <input
+            type="text"
+            value={word}
+            placeholder="English word"
+            required
+            onInput={(event) => {
+              setWord(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            value={translation}
+            placeholder="Translation"
+            required
+            onInput={(event) => {
+              setTranslation(event.target.value);
+            }}
+          />
+          <input
+            type="button"
+            value="+ Add explaining"
+            onClick={() => {
+              setOpen(true);
+            }}
+          />
+          <input type="submit" value="Add new one" />
+        </form>
+      </span>
+      <div className={`add-words__model ${isOpen ? "" : "none"}`}>
+        <div onClick={() => setOpen(false)}>Add</div>
+        <textarea
+          value={explaining}
+          placeholder="Explaining"
+          required
+          onInput={(event) => {
+            setExplaining(event.target.value);
+          }}
+          cols="30"
+          rows="10"
+        />
+      </div>
+    </div>
+  );
+}
